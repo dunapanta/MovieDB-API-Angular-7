8446533301001b1ffbdc6d37330272a1a72e92a3
Daniel Unapanta <dunapanta2@gmail.com>
2019-02-05 16:33:36 -0500

    algunos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MoviedbService } from "./services/moviedb.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Angular-MovieDB-API';
+  title = "Angular-MovieDB-API";
+
+  constructor(public _ms: MoviedbService) {
+    this._ms.getDiscoverMovies().subscribe(data => console.log(data));
+  }
 }
--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+// Importo map reactive extentions
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MoviedbService {
+  private apikey: string = "f9b276a8a665a41333c2def2f632a2e4";
+  private urlMoviedb: string = "https://api.themoviedb.org/3";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getDiscoverMovies() {
+    const url = `${
+      this.urlMoviedb
+    }/discover/movie?sort_by=popularity.desc&api_key=${
+      this.apikey
+    }&language=es&callback=JSONP_CALLBACK`;
+    return this.http.jsonp(url, "").pipe(map(res => res));
+  }
+
+  /*   getQuery(query: string) {
+    const url = `https://api.themoviedb.org/3${query}&api_key=${
+      this.apikey
+    }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.http.get(url);
+  } */
+
+  /*   getDiscoverMovies() {
+    return this.getQuery("/discover/movie?sort_by=popularity.desc").pipe(
+      map((data: any) => data.results)
+    );
+  } */
 }
