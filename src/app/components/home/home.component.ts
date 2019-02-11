eb7bd3246d526cf2bb7170d0db69d98321952eb7
Daniel Unapanta <dunapanta2@gmail.com>
2019-02-11 10:03:53 -0500

    Implementacion de despliegue de  Nuevas Peliculas en HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MoviedbService } from '../../services/moviedb.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  nuevasPeliculas: any[] = [];
+  loading: boolean;
 
-  ngOnInit() {
+  constructor(private moviedb: MoviedbService) {
+
+    this.loading = true;
+
+    this.moviedb.getDiscoverMovies()
+      .subscribe((data: any) => {
+
+        console.log(data);
+        this.nuevasPeliculas = data;
+        this.loading = false;
+      });
+
   }
+
 
 }
